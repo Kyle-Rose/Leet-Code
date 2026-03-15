525db719313cda5ad43af4a7a0022973c3bebfe7
Kyle-Rose <kyle.rose1@snhu.edu>
2026-03-15 17:03:53 -0400

    more efficient solution

--- two-sum.js
+++ two-sum.js
@@ -4,11 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map = new Map();
+
     for(let i = 0; i < nums.length; i++){
-        for (let j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i, j];
-            }
+        let current = nums[i];
+        let compliment = target - current;
+        if (map.has(compliment)){
+            return ([map.get(compliment), i])
+        }
+        else{
+            map.set(current, i);
         }
     }
 };
